68d812d74e1cad9777de8a074ae8b00db793c580
Kaminskiy Simon <kaminskii.simon@gmail.com>
2023-12-10 20:18:14 +0300

    Added Meduza Scraper, notebook with TG firts steps

--- src/solid_scraper.py
+++ src/solid_scraper.py
@@ -1,7 +1,9 @@
 """
 This script implemets scraping interfaces for news websites 
 """
+import asyncio
 import datetime
+import unicodedata
 
 from abc import ABC
 from abc import abstractmethod
@@ -12,24 +14,26 @@ import requests
 
 from bs4 import BeautifulSoup
 from loguru import logger
+from telethon.sync import TelegramClient
 
 
 class ABCScraper(ABC):
     def __init__(
         self,
-        rss_link: str,
+        source: str,
     ):
-        """Init func keeping link to RSS feed
+        """Init func keeping link to RSS feed or another source reference
 
         Args:
-            rss_link (str): link to RSS feed
+            source (str): link to RSS feed or another source reference
         """
-        self.rss_link = rss_link
+        self.source = source
         return
 
     @abstractmethod
     def get_latest_news(self) -> pd.DataFrame:
-        """Func to get table with all news from current RSS feed structured
+        """Func to get table with all news from current
+        RSS feed structured or telegram channel
 
         Returns
         -------
@@ -41,7 +45,8 @@ class ABCScraper(ABC):
 
     @abstractmethod
     def _get_news(self) -> list:
-        """Func to request current RSS feed and find all peieces of news
+        """Func to request current RSS feed or last XX news
+        from telegram channel and find all peieces of news
 
         Returns
         -------
@@ -92,18 +97,19 @@ class ABCScraper(ABC):
 class GeneralScraper(ABCScraper):
     def __init__(
         self,
-        rss_link: str,
+        source: str,
     ):
-        """Init func keeping link to RSS feed
+        """Init func keeping link to RSS feed or another source reference
 
         Args:
-            rss_link (str): link to RSS feed
+            source (str): link to RSS feed or another source reference
         """
-        self.rss_link = rss_link
+        self.source = source
         return
 
     def get_latest_news(self) -> pd.DataFrame:
-        """Func to get table with all news from current RSS feed structured
+        """Func to get table with all news from current
+        RSS feed structured or telegram channel
 
         Returns
         -------
@@ -111,7 +117,7 @@ class GeneralScraper(ABCScraper):
             table with data regarding each piece of new including its title,
             link, publication time and text itself
         """
-        news = self._get_news()
+        news = self._get_news(self.source)
         news_data = {
             "title": [],
             "link": [],
@@ -120,28 +126,60 @@ class GeneralScraper(ABCScraper):
             "source": [],
         }
         for piece_of_new in news:
-            news_data["title"].append(piece_of_new.find("title").text)
-            news_data["link"].append(piece_of_new.find("guid").text)
+            news_data["title"].append(self._get_title(piece_of_new))
+            news_data["link"].append(self._get_link(piece_of_new))
             news_data["publication_time"].append(
                 self._get_publication_time(piece_of_new)
             )
             news_data["text"].append(self._get_text(piece_of_new))
-            news_data["source"].append(self.rss_link)
+            news_data["source"].append(self.source)
         return pd.DataFrame.from_dict(news_data)
 
-    def _get_news(self) -> list:
-        """Func to request current RSS feed and find all peieces of news
+    def _get_news(self, source) -> list:
+        """Func to request current RSS feed structured
+        or telegram channel and find all peieces of news
 
         Returns
         -------
         list
-            list containing html pieces of news
+            list containing html or text pieces of news
         """
-        html_page = requests.get(self.rss_link).text
-        html_page = BeautifulSoup(html_page, "html.parser")
+        html_page = requests.get(source)
+        html_page.encoding = html_page.apparent_encoding
+        html_page = BeautifulSoup(html_page.text, "html.parser")
         news = html_page.findAll("item")
         logger.info(f"Length of news {len(news)}")
         return news
+
+    def _get_title(
+        self,
+        piece_of_new: bs4.element.Tag,
+    ) -> str:
+        """Func to get title of piece of new
+
+        Args:
+            piece_of_new (bs4.element.Tag): html piece of
+            new to extract the date and time
+
+        Returns:
+            str: title of piece of new
+        """
+        return piece_of_new.find("title").text
+
+    def _get_link(
+        self,
+        piece_of_new: bs4.element.Tag,
+    ) -> str:
+        """Func to get link of piece of new
+
+        Args:
+            piece_of_new (bs4.element.Tag): html
+            piece of new to extract the date and time
+
+        Returns:
+            str: link to piece of new
+        """
+        return piece_of_new.find("guid").text
 
     def _get_publication_time(
         self,
@@ -215,6 +253,7 @@ class RTScraper(GeneralScraper):
 
 class RIAScraper(GeneralScraper):
     def __init__(self):
+        """_summary_"""
         super().__init__("https://ria.ru/export/rss2/index.xml")
 
     def _get_text(
@@ -241,6 +280,7 @@ class RIAScraper(GeneralScraper):
 
 class VedomostiScraper(GeneralScraper):
     def __init__(self):
+        """_summary_"""
         super().__init__("https://www.vedomosti.ru/rss/news.xml")
 
     def _get_text(
@@ -263,3 +303,127 @@ class VedomostiScraper(GeneralScraper):
         )
         description = "".join([text.text for text in set_of_texts])
         return description
+
+
+class MeduzaScraper(GeneralScraper):
+    def __init__(self):
+        """_summary_"""
+        super().__init__("https://meduza.io/rss2/all")
+
+    def _get_title(self, piece_of_new: bs4.element.Tag) -> str:
+        """_summary_
+
+        Args:
+            piece_of_new (bs4.element.Tag): _description_
+
+        Returns:
+            str: _description_
+        """
+        title = piece_of_new.findAll("description")[0].find(
+            text=lambda tag: isinstance(tag, bs4.CData)
+        )
+        title = (
+            BeautifulSoup(title, "html.parser", from_encoding="utf-8").find("p").text
+        )
+        return unicodedata.normalize("NFKD", title)
+
+    def _get_text(self, piece_of_new: bs4.element.Tag) -> str:
+        """_summary_
+
+        Args:
+            piece_of_new (bs4.element.Tag): _description_
+
+        Returns:
+            str: _description_
+        """
+        full_text = piece_of_new.findAll("content:encoded")[0].find(
+            text=lambda tag: isinstance(tag, bs4.CData)
+        )
+        full_text = BeautifulSoup(
+            full_text, "html.parser", from_encoding="utf-8"
+        ).findAll("p")
+        full_text = " ".join([text.text for text in full_text])
+        return unicodedata.normalize("NFKD", full_text)
+
+
+# NOT WORKING, TO BE DONE
+class TGScraper(GeneralScraper):
+    def __init__(self):
+        """_summary_"""
+        super().__init__("Осторожно, новости")
+        self.tg_client = self.authenticate()
+        print("Successfully logged out")
+
+    def authenticate(self):
+        """_summary_"""
+        ioloop = asyncio.get_event_loop()
+        tasks = [
+            ioloop.create_task(
+                self._authenticate(
+                    28452758,
+                    "e33c7375760762a5dbc1ac8ed0a3c472",
+                    "79131292961",
+                )
+            )
+        ]
+        wait_tasks = asyncio.wait(tasks)
+        ioloop.run_until_complete(wait_tasks)
+
+    async def _authenticate(
+        self,
+        api_id: int,
+        api_hash: str,
+        phone: str,
+    ) -> TelegramClient:
+        """_summary_
+
+        Args:
+            api_id (int): _description_
+            api_hash (str): _description_
+            phone (str): _description_
+
+        Returns:
+            TelegramClient: _description_
+        """
+        tg_client = TelegramClient("TGScraper", api_id, api_hash)
+        await tg_client.start()
+        await tg_client.connect()
+        if not await tg_client.is_user_authorized():
+            await tg_client.send_code_request(phone)
+            await tg_client.sign_in(phone, input("Enter the code: "))
+        self.tg_client = tg_client
+
+    async def _logout(self, client: TelegramClient):
+        """_summary_
+
+        Args:
+            client (TelegramClient): _description_
+        """
+        client = await client
+        client.log_out()
+
+    async def _get_news(
+        self,
+        source,
+    ) -> list:
+        """something
+
+        Args:
+            source (_type_): something
+
+        Returns:
+            list: something
+        """
+        dialogs = await self.client.get_dialogs()
+        news = []
+        for dialog in dialogs:
+            if dialog.title == source:
+                messages = self.tg_client.iter_messages(dialog)
+                i = 0
+                async for message in messages:
+                    i += 1
+                    news.append(message)
+                    if i == 50:
+                        break
+        print(news[0])
+        return news
